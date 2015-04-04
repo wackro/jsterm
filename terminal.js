f84da49428f0ac35bc4b6b3ee209c01c30a0e3d7
Joe Griffin <wackro@gmail.com>
2015-04-04 03:26:19 +0100

    implement object-based commands; refactor everything

--- terminal.js
+++ terminal.js
@@ -11,27 +11,6 @@ var loggedIn = false;
 var prompt = { plain: "user@jgriff.in:~$ ", html: "user@jgriff.in:~$&nbsp;" }
 var github_repo = "http://github.com/wackro/homepage-2015";
 
-var commands = [
-	{
-		name: "about",
-		action: about,
-		usage: "about",
-		help: "this is the help for about"
-	},
-	{
-		name: "contact",
-		action: contact,
-		usage: "contact [-m [linkedin|mail|facebook]]",
-		help: "this is the help for contact"
-	},
-	{
-		name: "help",
-		action: help,
-		usage: "help [command]",
-		help: "there is no help for help..."
-	}
-];
-
 $(document).ready( function() {
 	init();
 });
@@ -51,17 +30,22 @@ function init() {
 	initText_1();
 }
 
-function run(action, args) {
-	var i;
-	for(i=0;i<commands.length;i++) {
-		if(commands[i].name == action) {
-			var matchingFunction = window[action];
-			if(typeof matchingFunction === "function")
-				matchingFunction(args);
-				return;
+function run(args) {
+	var action = getAction(args[0]);
+	if(action != null)
+		return action(args);
+	return(commandNotFound(args[0]));
+}
+
+function getAction(command) {	
+	for(var i=0;i<commands.length;i++) {
+		if(commands[i].name == command) {
+			var action = window[command];
+			if(typeof action === "function")
+				return action;
 		}
 	}
-	commandNotFound(action);
+	return null;
 }
 
 function registerListeners() {
@@ -80,25 +64,25 @@ function pause(f, quanta) {
 }
 
 function initText_1() {
-	addText("Login as: user", true);
+	write("Login as: user", true);
 	pause("initText_2()", 1);
 }
 
 function initText_2() {
-    addText("user@jgriff.in's password: ", true);
+	write("user@jgriff.in's password: ", true);
 	pause("initText_3()", 2);
 }
 
 function initText_3() {
-	addText("Welcome to jgriff.in", true);
-	addText("", true);
-	addText(" * Documentation: <a href='" + github_repo + "' target='_blank'>" + github_repo + "</a>", true);
+	write("Welcome to jgriff.in", true);
+	write("", true);
+	write(" * Documentation: <a href='" + github_repo + "' target='_blank'>" + github_repo + "</a>", true);
 	if($.cookie("lastLogin") == undefined) {
 		var date = new Date();
 		$.cookie("lastLogin", date.toString());
 	}
-	addText("Last Login: " + $.cookie("lastLogin"), true)
-	addText("", false);
+	write("Last Login: " + $.cookie("lastLogin"), true)
+	write("", false);
 	loggedIn = true;
 	terminalPrompt.show();
 	terminalCaret.show();
@@ -106,7 +90,7 @@ function initText_3() {
 }
 
 function initText_4() {
-	type("help", 1, true);
+	type("help", 0.2, true);
 }
 
 var currentChar = 0, timer;
@@ -125,71 +109,22 @@ function type(command, speed, hitEnter) {
 	timer = setInterval(type(command, speed, hitEnter), speed);
 }
 
-function addText(text, suppressUserString) {
+function write(text) {
 	terminalTop.scrollTop(terminalTop.height());
 	if(text != undefined) {
-		if(loggedIn && !suppressUserString) {
-			terminalTop.html(terminalTop.html() + "<b>" + prompt.plain + "</b>" + text);
-		}
-		else {
-			terminalTop.html(terminalTop.html() + text);
-		}
+		terminalTop.html(terminalTop.html() + text);
 	}
-
 	terminalTop.html(terminalTop.html() + "<br/>");
 	terminalBottom.focus();
 }
 
 function interpret(text) {
-	var splitText = text.split(' ')
-	addText(terminalBottom.text());
-	terminalBottom.text("", true);
-	run(splitText[0]);
-}
-
-var contact = function(args) {
-	var contactDetails = { 
-		facebook: { name: "facebook", link: "http://facebook.com/griffdogg" },
-		mail: { name: "mail", link: "mailto:wackro@gmail.com" },
-		linkedin: { name: "linkedin", link: "https://uk.linkedin.com/pub/joe-griffin/1b/9b2/25a" }
-	};
-	if(args.length == 3 && args[1] == "-m") {
-		switch(args[2].toLowerCase()) {
-			case "linkedin":
-				window.open(contactDetails.linkedin.link, "_blank");
-				return;
-			case "mail":
-				window.location.href = contactDetails.mail.link;
-				return;
-			case "facebook":
-				window.open(contactDetails.facebook.link, "_blank")
-				return;
-			default:
-				addText("contact [-m &lt;linkedin|email|facebook&gt;]", true);
-				addText();
-				return;
-		}
-	}
-	addText("<a href='" + contactDetails.linkedin.link + "' target='_blank'>Linkedin</a>", true);
-	addText("<a href='" + contactDetails.mail.link + "' target='_blank'>Mail</a>", true);
-	addText("<a href='" + contactDetails.facebook.link + "' target='_blank'>Facebook</a>", true);
-	addText();
-}
-
-var about = function(args) {
-	addText("Joe Griffin", true);
-	addText("London", true);
-	addText("Software Development:", true);
-	addText("&nbsp;&nbsp;Day time - C#", true);
-	addText("&nbsp;&nbsp;Night time - Python, JavaScript", true);
-	addText();
-}
-
-var help = function(args) {
-	addText("you asked for help", true);
-	addText();
+	var args = text.split(' ')
+	write("<b>" + terminalPrompt.text() + "</b>" + terminalBottom.text() + "<br/>" + run(args) + "<br/>");
+	terminalBottom.text("");
 }
 
 function commandNotFound(command) {
-	addText(command + ": command not found. Type `help' for a list of commands", true)
+	return command + ": command not found. Type `help' for a list of commands";
 }
+
