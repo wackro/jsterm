41913da0302e70c9b816790a1ee78e0ff1113244
wackro <wackro@gmail.com>
2015-06-20 14:19:05 +0100

    various refactors

--- commands.js
+++ commands.js
@@ -1,100 +1,75 @@
-var commands = [
-	{
+var x = [
+    {
 		name: "about",
 		action: function(args) {
-							if(args.length > 1) {
-								return about.usage
-							}
-							var output = "Joe Griffin<br/>London<br/>Software Developer:<br/>Daytime: C#<br/>Nighttime: Python/JS"
-							return output
-						},
+					if(args.length > 1) {
+						return about.usage
+					}
+					var output = "Joe Griffin<br/>London<br/>Software Developer:<br/>Daytime: C#<br/>Nighttime: Python/JS"
+					return output
+				},
 		usage: "about",
 		help: "Displays information about the owner of this page."
 	},
 	{
 		name: "contact",
 		action: function(args) {
-							var details = { 
-								facebook: { name: "facebook", link: "http://facebook.com/griffdogg" },
-								mail: { name: "mail", link: "mailto:wackro@gmail.com" },
-								linkedin: { name: "linkedin", link: "https://uk.linkedin.com/pub/joe-griffin/1b/9b2/25a" }
-							}
-							if(args.length == 1) {
-								var output = ""
-								output += "<a href='" + details.linkedin.link + "' target='_blank'>Linkedin</a><br/>" +
-										"<a href='" + details.mail.link + "' target='_blank'>Mail</a><br/>" +
-										"<a href='" + details.facebook.link + "' target='_blank'>Facebook</a>"
-								return output
-							}
-							else if(args.length == 2) {
-								switch(args[1].toLowerCase()) {
-									case "linkedin":
-										window.open(details.linkedin.link, "_blank")
-										break
-									case "mail":
-										window.location.href = details.mail.link
-										break
-									case "facebook":
-										window.open(details.facebook.link, "_blank")
-										break
-									default:
-										return contact.usage
-								}
-								return "Opening widow..."
-							}
-							else {
-								return contact.usage;
-							}
-						},
+			var details = { 
+				facebook: { name: "facebook", link: "http://facebook.com/griffdogg" },
+				mail: { name: "mail", link: "mailto:wackro@gmail.com" },
+				linkedin: { name: "linkedin", link: "https://uk.linkedin.com/pub/joe-griffin/1b/9b2/25a" }
+			}
+			if(args.length == 1) {
+				var output = ""
+				output += "<a href='" + details.linkedin.link + "' target='_blank'>Linkedin</a><br/>" +
+						"<a href='" + details.mail.link + "' target='_blank'>Mail</a><br/>" +
+						"<a href='" + details.facebook.link + "' target='_blank'>Facebook</a>"
+				return output
+			}
+			else if(args.length == 2) {
+				switch(args[1].toLowerCase()) {
+					case "linkedin":
+						window.open(details.linkedin.link, "_blank")
+						break
+					case "mail":
+						window.location.href = details.mail.link
+						break
+					case "facebook":
+						window.open(details.facebook.link, "_blank")
+						break
+					default:
+						return contact.usage
+				}
+				return "Opening widow..."
+			}
+			else {
+				return contact.usage;
+			}
+		},
 		usage: "contact [linkedin|mail|facebook]",
 		help: "Displays information about getting hold of the owner of this page.<br/><br/>If a method is specified, carries out that method of contact immediately, otherwise a list of contact details is printed."
 	},
 	{
-		name: "help",
-		action: function(args) {
-							if(args.length == 1) {
-								var output = "JS terminal, version 0.1<br/>" +
-											"These shell commands are defined internally.  Type `help' to see this list.<br/>" +
-											"Type `help name' to find out more about the function `name'.<br/><br/>"
-
-								$.each(commands, function(index, command) {
-									output += command.usage
-									if(index != commands.length-1)
-										output += "<br/>"
-								})
-								return output
-							}
-							else {
-								var command = getCommand(args[1])
-								if(command != null)
-									return command.help;
-								return "help: no help topics match " + args [1]
-							}
-						},
-		usage: "help [command]",
-		help: "Displays information about builtin commands.<br/><br/>If a command is specified, gives detailed help on that command, otherwise the list of help topics is printed."
-	},
-	{
 		name: "cv",
 		action: function(args) {
-							return "<a href='curriculum-vitae-joe-griffin.pdf' target='_blank'>Read it, absorb it.</a>"
-						},
+			return "<a href='curriculum-vitae-joe-griffin.pdf' target='_blank'>Read it, absorb it.</a>"
+		},
 		usage: "cv",
 		help: "Provides a link to my CV."
 	},
 	{
 		name: "photography",
 		action: function(args) {
-							return "<a href='/photography' target='_blank'>Take me there.</a>"
-						},
+			return "<a href='/photography' target='_blank'>Take me there.</a>"
+		},
 		usage: "photography",
 		help: "Provides a link to my photography portfolio."
 	},
 	{
 		name: "easymode",
 		action: function(args) {
-							return "<a href='/easymode'>Get me out of here.</a>"
-						},
+			return "<a href='/easymode'>Get me out of here.</a>"
+		},
 		usage: "<a href='/easymode'>easymode</a>",
 		help: "Provides a link to get you the hell out of here."
 	}
--- terminal.js
+++ terminal.js
@@ -1,41 +1,67 @@
-/**
- * An interactive terminal-inspired web application for the homepage of jgriff.in.
+/*
+ * An interactive terminal-inspired command line parser.
  * 
  * http://github.com/wackro/homepage-2015
  */
 
 var terminal, terminalBottom, terminalTop, terminalPrompt, terminalCaret;
 
-var loggedIn = false;
 var prompt = { plain: "user@jgriff.in:~$ ", html: "user@jgriff.in:~$&nbsp;" }
-var github_repo = "http://github.com/wackro/homepage-2015";
-var currentHistoryitem = 0;
-var commandHistory = [];
+var github_repo = "http://github.com/wackro/homepage-2015"
+var currentHistoryitem = 0
+var commandHistory = []
+var commands = [{
+		name: "help",
+		action: function(args) {
+			if(args.length == 1) {
+				var output = "JS terminal, version 0.1<br/>" +
+							"These shell commands are defined internally.  Type `help' to see this list.<br/>" +
+							"Type `help name' to find out more about the function `name'.<br/><br/>"
+				
+				$.each(commands, function(index, command) {
+					output += command.usage
+					if(index != commands.length-1)
+						output += "<br/>"
+				})
+				return output
+			}
+			else {
+				var command = getCommand(args[1])
+				if(command != null)
+					return command.help;
+				return "help: no help topics match " + args [1]
+			}
+		},
+		usage: "help [command]",
+		help: "Displays information about builtin commands.<br/><br/>If a command is specified, gives detailed help on that command, otherwise the list of help topics is printed."
+	}]
 
 $(document).ready( function() {
-	init();
+	init()
 });
 
 function init() {
-	terminal = $("div#terminal");
+	terminal = $("div#terminal")
 	terminal.addClass("terminal")
-	terminal.append("<div id='terminal-top' class='terminal'></div>");
+	terminal.append("<div id='terminal-top' class='terminal'></div>")
 	terminal.append("<span id='terminal-prompt'>" + prompt.html + "</span>"
 		+ "<span id='terminal-bottom' class='terminal' spellCheck='false' contentEditable></span>"
-		+ "<span id='terminal-caret'>&nbsp;</span>");
-	terminalPrompt = terminal.find("#terminal-prompt");
+		+ "<span id='terminal-caret'>&nbsp;</span>")
+	terminalPrompt = terminal.find("#terminal-prompt")
 	terminalCaret = terminal.find("#terminal-caret")
-	terminalTop = $("#terminal #terminal-top");
-	terminalBottom = $("#terminal #terminal-bottom");
+	terminalTop = $("#terminal #terminal-top")
+	terminalBottom = $("#terminal #terminal-bottom")
 	registerListeners()
-	initText_1();
+	terminalPrompt.show()
+	terminalCaret.show()
+	type("help", 0.2, true)
 }
 
 function run(args) {
-	var command = getCommand(args[0]);
+	var command = getCommand(args[0])
 	if(command != null)
-		return command.action(args);
-	return(commandNotFound(args[0]));
+		return command.action(args)
+	return(commandNotFound(args[0]))
 }
 
 function getCommand(command) {	
@@ -43,119 +69,114 @@ function getCommand(command) {
 		if(commands[i].name == command)
 			return commands[i]
 	}
-	return null;
+	return null
 }
 
 function registerListeners() {
 	terminalBottom.keydown(function(e) {
 		switch(e.keyCode) {
 			case 13:	// enter
-				interpret(terminalBottom.text());
-				break;
+				interpret(terminalBottom.text())
+				break
 			case 38:	// up
-				historyBack();
-				break;
+				historyBack()
+				break
 			case 40:	// down
-				historyForward();
-				break;
+				historyForward()
+				break
 		}
 	});
 	terminalBottom.focusout(function(e) {
-		terminalBottom.focus();
+		terminalBottom.focus()
 	});
 }
 
 function historyBack() {
 	if(currentHistoryitem == commandHistory.length)
-		return;
-	currentHistoryitem++;
-	terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem]);
+		return
+	currentHistoryitem++
+	terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem])
 }
 
 function historyForward() {
 	if(terminalBottom.text() == "")
-		return;
-	currentHistoryitem--;
-	terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem] || "");
+		return
+	currentHistoryitem--
+	terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem] || "")
 }
 
 function pause(f, quanta) {
-	window.setTimeout(f, quanta*1000);
+	window.setTimeout(f, quanta*1000)
 }
 
-function initText_1() {
-	write("Login as: user");
-	pause("initText_2()", 1);
-}
-
-function initText_2() {
-	write("user@jgriff.in's password: ", true);
-	pause("initText_3()", 2);
-}
-
-function initText_3() {
-	write("Welcome to jgriff.in<br/><br/>" +
-			"* Documentation: <a href='" + github_repo + "' target='_blank'>" + github_repo + "</a>");
-	if($.cookie("lastLogin") == undefined) {
-		var date = new Date();
-		$.cookie("lastLogin", date.toString());
-	}
-	write("Last Login: " + $.cookie("lastLogin"));
-	loggedIn = true;
-	terminalPrompt.show();
-	terminalCaret.show();
-	pause("initText_4()", 1);
-}
-
-function initText_4() {
-	type("help", 0.2, true);
-}
-
-var currentChar = 0;
+var currentChar = 0
 function type(text, speed, hitEnter) {
 	if(currentChar == text.length) {
-		currentChar = 0;
+		currentChar = 0
 		if(hitEnter) {
-			var e = jQuery.Event("keydown");
-			e.keyCode = 13;
-			terminalBottom.trigger(e);
+			var e = jQuery.Event("keydown")
+			e.keyCode = 13
+			terminalBottom.trigger(e)
 		}
-		return;
+		return
 	}
-	terminalBottom.text(terminalBottom.text() + text[currentChar++]);
-	pause(function(){type(text, speed, hitEnter)}, speed);
+	terminalBottom.text(terminalBottom.text() + text[currentChar++])
+	pause(function(){type(text, speed, hitEnter)}, speed)
 }
 
 function write(text) {
 	if(text != undefined) {
-		terminalTop.html(terminalTop.html() + text);
+		terminalTop.html(terminalTop.html() + text)
 	}
-	terminalTop.html(terminalTop.html() + "<br/>");
-	terminalTop.scrollTop(terminalTop.prop("scrollHeight"));
-	terminalBottom.focus();
+	terminalTop.html(terminalTop.html() + "<br/>")
+	terminalTop.scrollTop(terminalTop.prop("scrollHeight"))
+	terminalBottom.focus()
 }
 
 function interpret(text) {
-	currentHistoryitem = 0;
-	var args = text.split(' ');
+	currentHistoryitem = 0
+	var args = text.split(' ')
 	if(text.trim() == "") {
-		write("<b>" + terminalPrompt.text() + "</b>");
+		write("<b>" + terminalPrompt.text() + "</b>")
 	}
 	else {
-		addHistory(text);
-		write("<b>" + terminalPrompt.text() + "</b>" + terminalBottom.text() + "<br/>" + run(args) + "<br/>");
+		addHistory(text)
+		write("<b>" + terminalPrompt.text() + "</b>" + terminalBottom.text() + "<br/>" + run(args) + "<br/>")
 	}
-	terminalBottom.text("");
+	terminalBottom.text("")
 }
 
 function addHistory(text) {
 	text = text.trim()
 	if(text != "" && commandHistory.length == 0 || 
 		(commandHistory.length >=1 && commandHistory[commandHistory.length-1] != text))
-		commandHistory.push(text);
+		commandHistory.push(text)
 }
 
 function commandNotFound(command) {
-	return command + ": command not found. Type `help' for a list of commands";
+	return command + ": command not found. Type `help' for a list of commands"
 }
 
+function registerCommands(commands) {
+	if(!Array.isArray(commands))
+		return
+	$.each(commands, function(index, command) {
+		registerCommand(command)
+	})
+}
+
+function registerCommand(command) {
+	if(isCommandValid(command))
+		commands.push(command)
+	else
+		console.log("Couldn't register command " + command)
+}
+
+function isCommandValid(command) {
+	if(typeof command.name === "undefined" ||
+		typeof command.usage === "undefined" ||
+		typeof command.action === "undefined" ||
+		typeof command.help === "undefined")
+		return false
+	return true
+}
