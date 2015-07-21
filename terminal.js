a922d97a63afe1ac268a16e42b3e993e62c01fe7
wackro <wackro@gmail.com>
2015-07-22 00:12:52 +0100

    just committing this idc

--- terminal.js
+++ terminal.js
@@ -4,13 +4,11 @@
  * http://github.com/wackro/homepage-2015
  */
 
-var terminal, terminalBottom, terminalTop, terminalPrompt, terminalCaret;
-
-var prompt = { plain: "user@jgriff.in:~$ ", html: "user@jgriff.in:~$&nbsp;" }
-var github_repo = "http://github.com/wackro/homepage-2015"
+var terminal, terminalBottom, terminalTop, terminalPrompt, terminalCaret
+var prompt
 var currentHistoryitem = 0
 var commandHistory = []
-var commands = [{
+var globalCommands = [{
 		name: "help",
 		action: function(args) {
 			if(args.length == 1) {
@@ -18,9 +16,9 @@ var commands = [{
 							"These shell commands are defined internally.  Type `help' to see this list.<br/>" +
 							"Type `help name' to find out more about the function `name'.<br/><br/>"
 				
-				$.each(commands, function(index, command) {
+				$.each(globalCommands, function(index, command) {
 					output += command.usage
-					if(index != commands.length-1)
+					if(index != globalCommands.length-1)
 						output += "<br/>"
 				})
 				return output
@@ -36,14 +34,11 @@ var commands = [{
 		help: "Displays information about builtin commands.<br/><br/>If a command is specified, gives detailed help on that command, otherwise the list of help topics is printed."
 	}]
 
-$(document).ready( function() {
-	init()
-});
-
-function init() {
-	terminal = $("div#terminal")
+function init(container, prompt) {
+	terminal = $(container)
 	terminal.addClass("terminal")
 	terminal.append("<div id='terminal-top' class='terminal'></div>")
+    this.prompt = prompt
 	terminal.append("<span id='terminal-prompt'>" + prompt.html + "</span>"
 		+ "<span id='terminal-bottom' class='terminal' spellCheck='false' contentEditable></span>"
 		+ "<span id='terminal-caret'>&nbsp;</span>")
@@ -54,22 +49,22 @@ function init() {
 	registerListeners()
 	terminalPrompt.show()
 	terminalCaret.show()
-	type("help", 0.2, true)
+    terminalBottom.focus()
 }
 
 function run(args) {
 	var command = getCommand(args[0])
 	if(command != null)
 		return command.action(args)
-	return(commandNotFound(args[0]))
-}
-
-function getCommand(command) {	
-	for(var i=0;i<commands.length;i++) {
-		if(commands[i].name == command)
-			return commands[i]
-	}
-	return null
+	return args[0] + ": command not found. Type `help' for a list of commands"
+    
+    function getCommand(command) {	
+	    for(var i=0;i<globalCommands.length;i++) {
+		    if(globalCommands[i].name == command)
+			    return globalCommands[i]
+	    }
+	    return null
+    }
 }
 
 function registerListeners() {
@@ -89,48 +84,20 @@ function registerListeners() {
 	terminalBottom.focusout(function(e) {
 		terminalBottom.focus()
 	});
-}
+    
+    function historyBack() {
+	    if(currentHistoryitem == commandHistory.length)
+		    return
+	    currentHistoryitem++
+	    terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem])
+    }
 
-function historyBack() {
-	if(currentHistoryitem == commandHistory.length)
-		return
-	currentHistoryitem++
-	terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem])
-}
-
-function historyForward() {
-	if(terminalBottom.text() == "")
-		return
-	currentHistoryitem--
-	terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem] || "")
-}
-
-function pause(f, quanta) {
-	window.setTimeout(f, quanta*1000)
-}
-
-var currentChar = 0
-function type(text, speed, hitEnter) {
-	if(currentChar == text.length) {
-		currentChar = 0
-		if(hitEnter) {
-			var e = jQuery.Event("keydown")
-			e.keyCode = 13
-			terminalBottom.trigger(e)
-		}
-		return
-	}
-	terminalBottom.text(terminalBottom.text() + text[currentChar++])
-	pause(function(){type(text, speed, hitEnter)}, speed)
-}
-
-function write(text) {
-	if(text != undefined) {
-		terminalTop.html(terminalTop.html() + text)
-	}
-	terminalTop.html(terminalTop.html() + "<br/>")
-	terminalTop.scrollTop(terminalTop.prop("scrollHeight"))
-	terminalBottom.focus()
+    function historyForward() {
+	    if(terminalBottom.text() == "")
+		    return
+	    currentHistoryitem--
+	    terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem] || "")
+    }
 }
 
 function interpret(text) {
@@ -144,39 +111,42 @@ function interpret(text) {
 		write("<b>" + terminalPrompt.text() + "</b>" + terminalBottom.text() + "<br/>" + run(args) + "<br/>")
 	}
 	terminalBottom.text("")
-}
 
-function addHistory(text) {
-	text = text.trim()
-	if(text != "" && commandHistory.length == 0 || 
-		(commandHistory.length >=1 && commandHistory[commandHistory.length-1] != text))
-		commandHistory.push(text)
-}
-
-function commandNotFound(command) {
-	return command + ": command not found. Type `help' for a list of commands"
+    function addHistory(text) {
+	    text = text.trim()
+	    if(text != "" && commandHistory.length == 0 || 
+		    (commandHistory.length >=1 && commandHistory[commandHistory.length-1] != text))
+		    commandHistory.push(text)
+    }
+    
+    function write(text) {
+	    if(text != undefined) {
+		    terminalTop.html(terminalTop.html() + text)
+	    }
+	    terminalTop.html(terminalTop.html() + "<br/>")
+	    terminalTop.scrollTop(terminalTop.prop("scrollHeight"))
+	    terminalBottom.focus()
+    }
 }
 
 function registerCommands(commands) {
 	if(!Array.isArray(commands))
 		return
 	$.each(commands, function(index, command) {
-		registerCommand(command)
+		if(isCommandValid(command))
+		    globalCommands.push(command)
+	    else
+		    console.log("Couldn't register command " + command)
 	})
+
+    function isCommandValid(command) {
+	    if(typeof command.name === "undefined" ||
+		    typeof command.usage === "undefined" ||
+		    typeof command.action === "undefined" ||
+		    typeof command.help === "undefined")
+		    return false
+	    return true
+    }
 }
 
-function registerCommand(command) {
-	if(isCommandValid(command))
-		commands.push(command)
-	else
-		console.log("Couldn't register command " + command)
-}
 
-function isCommandValid(command) {
-	if(typeof command.name === "undefined" ||
-		typeof command.usage === "undefined" ||
-		typeof command.action === "undefined" ||
-		typeof command.help === "undefined")
-		return false
-	return true
-}
