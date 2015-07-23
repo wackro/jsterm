33bfe271bbb8695bcfe1b63b83589ca4cbee06a4
Joe Griffin <wackro@gmail.com>
2015-07-23 10:10:33 +0100

    fix whitespace

--- terminal.js
+++ terminal.js
@@ -13,8 +13,8 @@ var globalCommands = [{
 		action: function(args) {
 			if(args.length == 1) {
 				var output = "JSTerm - <a href='http://github.com/wackro/jsterm'>http://github.com/wackro/jsterm</a><br/>" +
-							"These shell commands are defined internally.  Type `help' to see this list.<br/>" +
-							"Type `help name' to find out more about the function `name'.<br/><br/>"
+						"These shell commands are defined internally.  Type `help' to see this list.<br/>" +
+						"Type `help name' to find out more about the function `name'.<br/><br/>"
 				
 				$.each(globalCommands, function(index, command) {
 					output += command.usage
@@ -38,7 +38,7 @@ function init(container, prompt) {
 	terminal = $(container)
 	terminal.addClass("terminal")
 	terminal.append("<div id='terminal-top' class='terminal'></div>")
-    this.prompt = { plain: prompt, html: prompt.replace(" ", "&nbsp;") }
+	this.prompt = { plain: prompt, html: prompt.replace(" ", "&nbsp;") }
 	terminal.append("<span id='terminal-prompt'>" + this.prompt.html + "</span>"
 		+ "<span id='terminal-bottom' class='terminal' spellCheck='false' contentEditable></span>"
 		+ "<span id='terminal-caret'>&nbsp;</span>")
@@ -85,19 +85,19 @@ function registerListeners() {
 		terminalBottom.focus()
 	});
     
-    function historyBack() {
-	    if(currentHistoryitem == commandHistory.length)
+	function historyBack() {
+		if(currentHistoryitem == commandHistory.length)
 		    return
-	    currentHistoryitem++
-	    terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem])
-    }
-
-    function historyForward() {
-	    if(terminalBottom.text() == "")
+		currentHistoryitem++
+		terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem])
+	}
+	
+	function historyForward() {
+		if(terminalBottom.text() == "")
 		    return
-	    currentHistoryitem--
-	    terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem] || "")
-    }
+		currentHistoryitem--
+		terminalBottom.text(commandHistory[commandHistory.length - currentHistoryitem] || "")
+	}
 }
 
 function interpret(text) {
@@ -111,22 +111,22 @@ function interpret(text) {
 		write("<b>" + terminalPrompt.text() + "</b>" + terminalBottom.text() + "<br/>" + run(args) + "<br/>")
 	}
 	terminalBottom.text("")
-
-    function addHistory(text) {
-	    text = text.trim()
-	    if(text != "" && commandHistory.length == 0 || 
-		    (commandHistory.length >=1 && commandHistory[commandHistory.length-1] != text))
-		    commandHistory.push(text)
-    }
-    
-    function write(text) {
-	    if(text != undefined) {
+	
+	function addHistory(text) {
+		text = text.trim()
+		if(text != "" && commandHistory.length == 0 || 
+			(commandHistory.length >=1 && commandHistory[commandHistory.length-1] != text))
+			commandHistory.push(text)
+	}
+	
+	function write(text) {
+		if(text != undefined) {
 		    terminalTop.html(terminalTop.html() + text)
-	    }
-	    terminalTop.html(terminalTop.html() + "<br/>")
-	    terminalTop.scrollTop(terminalTop.prop("scrollHeight"))
-	    terminalBottom.focus()
-    }
+		}
+		terminalTop.html(terminalTop.html() + "<br/>")
+		terminalTop.scrollTop(terminalTop.prop("scrollHeight"))
+		terminalBottom.focus()
+	}
 }
 
 function registerCommands(commands) {
@@ -134,24 +134,24 @@ function registerCommands(commands) {
 		return
 	$.each(commands, function(index, command) {
 		if(isCommandValid(command))
-		    globalCommands.push(command)
-	    else
-		    console.log("Couldn't register command " + command)
+			globalCommands.push(command)
+		else
+		console.log("Couldn't register command " + command)
 	})
-    globalCommands.sort(sortCommandsByName)
-    
-    function isCommandValid(command) {
-	    if(typeof command.name === "undefined" ||
-		    typeof command.usage === "undefined" ||
-		    typeof command.action === "undefined" ||
-		    typeof command.help === "undefined")
-		    return false
-	    return true
-    }
-    
-    function sortCommandsByName(a, b) {
-        return (a.name > b.name) ? 1 : ( a.name < b.name ? -1 : 0)
-    }
+	globalCommands.sort(sortCommandsByName)
+	
+	function isCommandValid(command) {
+		if(typeof command.name === "undefined" ||
+			typeof command.usage === "undefined" ||
+			typeof command.action === "undefined" ||
+			typeof command.help === "undefined")
+			return false
+		return true
+	}
+	
+	function sortCommandsByName(a, b) {
+		return (a.name > b.name) ? 1 : ( a.name < b.name ? -1 : 0)
+	}
 }
 
 
